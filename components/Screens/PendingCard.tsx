a74df891c2e8d9e446a1be9430758021248cf0a1
mbeka02 <antonymbeka@gmail.com>
2024-08-21 12:51:36 +0300

    styling fixes for the pendingCard

--- components/Screens/PendingCard.tsx
+++ components/Screens/PendingCard.tsx
@@ -1,56 +1,50 @@
+import { View, StyleSheet } from "react-native";
+import { ThemedText } from "../ThemedText";
+import { useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
-import { View, Text, StyleSheet } from "react-native"
-import { ThemedText } from "../ThemedText"
-import { Link } from "expo-router"
-import { TouchableOpacity } from 'react-native';
-
-export function PendingCard(props: any) {
-
-    return (
-        <TouchableOpacity>
-            <Link href={{
-                pathname: '/pay/[pay]',
-                params: {pay: '1'}
-
-            }}>
-                <View style={styles.card}>
-                    <View style={styles.top}>
-                        <ThemedText style={{ color: "#36383F" }}>0x0123456</ThemedText>
-                        <ThemedText style={{ color: "#36383F" }}>12th July, 2024</ThemedText>
-                    </View>
-                    <View style={styles.bottom}>
-                        <ThemedText style={{ color: "#36383F" }}>Jumia...</ThemedText>
-                        <ThemedText style={{ color: "green" }}>0.458 APT</ThemedText>
-                    </View>
-
-                </View>
-            </Link>
-        </TouchableOpacity>
-
-    )
+export function PendingCard() {
+  const router = useRouter();
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        router.push({ pathname: "/pay/[pay]", params: { pay: "1" } })
+      }
+    >
+      <View style={styles.card}>
+        <View style={styles.top}>
+          <ThemedText style={{ color: "#36383F" }}>0x0123456</ThemedText>
+          <ThemedText style={{ color: "#36383F" }}>12th July, 2024</ThemedText>
+        </View>
+        <View style={styles.bottom}>
+          <ThemedText style={{ color: "#36383F" }}>Jumia...</ThemedText>
+          <ThemedText style={{ color: "green" }}>0.458 APT</ThemedText>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
 const styles = StyleSheet.create({
-    top: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 10
-    },
-    bottom: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    card: {
-        paddingVertical: 20,
-        paddingHorizontal: 25,
-        // width: '100%',
-        borderRadius: 20,
-        marginVertical: 10,
-        backgroundColor: '#C0C2C9',
-        elevation: 5,
-        // opacity: 0.5
-
-    },
-    amount: {
-        color: 'green'
-    }
-})
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  bottom: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  card: {
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    width: "100%",
+    borderRadius: 20,
+    marginVertical: 10,
+    backgroundColor: "#C0C2C9",
+    elevation: 5,
+    // opacity: 0.5
+  },
+  amount: {
+    color: "green",
+  },
+});
--- components/Screens/RequestCard.tsx
+++ components/Screens/RequestCard.tsx
@@ -1,49 +1,47 @@
-import { View, Text, StyleSheet } from "react-native"
-import { ThemedText } from "../ThemedText"
+import { View, Text, StyleSheet } from "react-native";
+import { ThemedText } from "../ThemedText";
 type Props = {
-    payeeAddress: string;
-    amount: number;
-    date: string;
-    reason: string;
-}
+  payeeAddress: string;
+  amount: number;
+  date: string;
+  reason: string;
+};
 
 export function RequestCard() {
-    return (
-        <View style={styles.card}>
-            <View style={styles.top}>
-                <ThemedText style={{ color: "#36383F" }}>0x0123456</ThemedText>
-                <ThemedText style={{ color: "#36383F" }}>12th July, 2024</ThemedText>
-            </View>
-            <View style={styles.bottom}>
-                <ThemedText style={{ color: "#36383F" }}>Jumia...</ThemedText>
-                <ThemedText style={styles.amount}>0.458 APT</ThemedText>
-            </View>
-
-        </View>
-    )
+  return (
+    <View style={styles.card}>
+      <View style={styles.top}>
+        <ThemedText style={{ color: "#36383F" }}>0x0123456</ThemedText>
+        <ThemedText style={{ color: "#36383F" }}>12th July, 2024</ThemedText>
+      </View>
+      <View style={styles.bottom}>
+        <ThemedText style={{ color: "#36383F" }}>Jumia...</ThemedText>
+        <ThemedText style={styles.amount}>0.458 APT</ThemedText>
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    top: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 10
-    },
-    bottom: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    card: {
-        paddingVertical: 20,
-        paddingHorizontal: 25,
-        width: '100%',
-        borderRadius: 20,
-        marginVertical: 10,
-        backgroundColor: '#C0C2C9',
-        elevation: 5,
-        // opacity: 0.5
-
-    },
-    amount: {
-        color: 'green'
-    }
-})
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  bottom: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  card: {
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    width: "100%",
+    borderRadius: 20,
+    marginVertical: 10,
+    backgroundColor: "#C0C2C9",
+    elevation: 5,
+    // opacity: 0.5
+  },
+  amount: {
+    color: "green",
+  },
+});
