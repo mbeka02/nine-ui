ac8b4ac1da354eb1560ee8917e2276981b916cf0
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-08-28 17:28:12 +0300

    feat: Connecting to petra wallet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Pressable, Text } from 'react-native';
+import '../../global'
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { RequestCard } from '@/components/Screens/RequestCard';
+import petra from '@/components/wallet/petra';
 export default function HomeScreen() {
     return (
         <ParallaxScrollView
         >
             <ThemedView style={styles.titleContainer}>
                 <ThemedText type="title">Home</ThemedText>
+                <Pressable style={styles.button} onPress={() => petra.connect()}>
+                    <Text style={styles.text}> Connect</Text>
+                </Pressable>
             </ThemedView>
             <RequestCard />
 
@@ -23,6 +27,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         gap: 4,
+    }, button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#0ABAB5',
+
     },
-   
+    text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    }
+
 });
