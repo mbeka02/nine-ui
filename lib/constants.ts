5bb19b394961bbafc2fc50c89b756f32a6fa1452
Roman Njoroge <roman.njoroge@njuguna.com>
2024-09-23 21:15:39 +0300

    test: Renaming variables for testing purposes

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
-export const USERS_PRIVATE_KEY = "users_private_key_v2";
+export const USERS_PRIVATE_KEY = "users_private_key_v5";
 export const DERIVATION_PATH = "m/44'/637'/0'/0'/0'"
-export const USER_ACCOUNT_MNEMONIC = "users_mnemonic_v2";
-export const HAS_SYNCED_USER_DETAILS = "has_synced_user_detais_v4";
+export const USER_ACCOUNT_MNEMONIC = "users_mnemonic_v5";
+export const HAS_SYNCED_USER_DETAILS = "has_synced_user_detais_v5";
 export const FALSE = "false";
 export const TRUE = "true";
