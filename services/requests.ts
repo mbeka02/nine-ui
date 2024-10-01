bf463bfc1d5cbd74ffa09f152f28d79d7242ac74
mbeka02 <antonymbeka@gmail.com>
2024-10-02 01:13:59 +0300

    feat:completed history screen

--- services/requests.ts
+++ services/requests.ts
@@ -26,6 +26,6 @@ export const getAllRequests = async (): Promise<GetRequestData[]> => {
     const data = await response.json();
     return data;
   } catch (error) {
-    throw new Error("something went wrong");
+    throw new Error("something went wrong ");
   }
 };
